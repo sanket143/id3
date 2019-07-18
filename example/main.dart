ca66b7cc8a008c6cd3b94ae413f782c2f17f4a8d
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2019-07-18 16:27:46 +0530

    Added Example

---
+++ example/main.dart
@@ -0,0 +1,21 @@
+import 'package:id3/id3.dart';
+
+void main(){
+  MP3Instance mp3instance = new MP3Instance("./file.mp3");
+  mp3instance.parseTags();
+
+  print(mp3instance.getMetaTags());
+}
+
+// {
+//   "Title": "SongName",
+//   "Artist": "ArtistName",
+//   "Album": "AlbumName",
+//   "APIC": {
+//     "mime": "image/jpeg",
+//     "textEncoding": "0",
+//     "picType": "0",
+//     "description": "description",
+//     "base64": "AP/Y/+AAEEpGSUYAAQEBAE..."
+//   }
+// }

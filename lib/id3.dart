5e7e6d69369983b59df5a11e993ab8305a05d17d
Callum Moffat <smartercallum@gmail.com>
2020-11-18 22:39:06 -0500

    Fix frame size calculation

--- lib/id3.dart
+++ lib/id3.dart
@@ -135,9 +135,9 @@ class MP3Instance {
 int parseSize(List<int> block) {
   assert(block.length == 4);
 
-  var len = block[0] << 21;
-  len += block[1] << 14;
-  len += block[2] << 7;
+  var len = block[0] << 24;
+  len += block[1] << 16;
+  len += block[2] << 8;
   len += block[3];
 
   return len;
